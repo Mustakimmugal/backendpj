d2804f8c213244bf4f3676fce9abd09a1e5b562a
Mustakim Mugal <mustakimmugal824@gmail.com>
2026-02-17 21:37:28 -0800

    Update api.js

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://backendpj.onrender.com/api/v1",
+  baseURL: "https://backendpj.onrender.com/api",
 });
 
 // Automatically token attach karega
