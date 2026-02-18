bbfd0f9313a339511526909201bf3186b8d6a7f3
Mustakim Mugal <mustakimmugal824@gmail.com>
2026-02-17 22:17:05 -0800

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -8,29 +8,22 @@ import taskRoutes from "./routes/taskRoutes.js";
 import errorHandler from "./middleware/errorMiddleware.js";
 
 dotenv.config();
-
-// Connect Database
 connectDB();
 
 const app = express();
 
-// Middleware
 app.use(cors());
 app.use(express.json());
 
-// Routes
 app.use("/api/auth", authRoutes);
 app.use("/api/tasks", taskRoutes);
 
-// Test Route
 app.get("/", (req, res) => {
   res.send("Backend API Running");
 });
 
-// Error Middleware (always last)
 app.use(errorHandler);
 
-// ✅ Important for deployment (Render / Railway etc.)
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
