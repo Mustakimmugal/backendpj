f1f67405feddef6259bf1050623d8338342470dc
Mustakim Mugal <mustakimmugal824@gmail.com>
2026-02-17 21:41:02 -0800

    Update authRoutes.js

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -6,7 +6,7 @@ import {
 
 const router = express.Router();
 
-router.post("/register", registerUser);
-router.post("/login", loginUser);
+router.post("/auth/register", registerUser);
+router.post("/auth/login", loginUser);
 
 export default router;
